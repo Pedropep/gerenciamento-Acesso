fffebb6f83659857a58d7ddaf3bbb6bae3a68c77
Cb Estevão <pedroestevaopep@gmail.com>
2023-12-05 15:47:56 -0300

    Criando componente registro

--- front/frontAcesso/src/app/app.module.ts
+++ front/frontAcesso/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PainelComponent } from './painel/painel.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelComponent        
+    PainelComponent,
+    RegistroComponent        
   ],
   imports: [
     BrowserModule,
