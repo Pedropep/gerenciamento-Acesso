13be4b88e86b82db6afb7cc7b50294250f0de5d7
Cb Estevão <pedroestevaopep@gmail.com>
2023-12-05 15:16:50 -0300

    Criando componente painel

--- front/frontAcesso/src/app/app.module.ts
+++ front/frontAcesso/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent        
   ],
   imports: [
     BrowserModule,
