83440e626e05f67a27c2ebcbafbccff99ba71460
Cb Estevão <pedroestevaopep@gmail.com>
2023-12-11 11:11:00 -0300

    Criando componente cadastro

--- front/frontAcesso/src/app/app.module.ts
+++ front/frontAcesso/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PainelComponent } from './painel/painel.component';
 import { RegistroComponent } from './registro/registro.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelComponent,
-    RegistroComponent        
+    RegistroComponent,
+    CadastroComponent        
   ],
   imports: [
     BrowserModule,
